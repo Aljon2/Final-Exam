89bd4a81982389b77c9e656961e17315bee10679
Aljon S. Sapilan <aljon.sapilan@evsu.edu.ph>
2022-01-07 00:54:29 +0800

    Simple Calculator

---
+++ Calculator/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Calculator';
+
+
+  public value1!:number;
+  public value2!:number;
+  public value3!:number;
+
+  add(){
+    this.value3=this.value1+this.value2;
+  }
+  sub(){
+    this.value3=this.value1-this.value2;
+  }
+  multiply(){
+    this.value3=this.value1*this.value2;
+  }
+  divide(){
+    this.value3=this.value1/this.value2;
+  }
+}
